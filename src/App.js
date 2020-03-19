893be6f3fc161adf146003a8b68fb4bc39ca888d
Matt Koenig <koenigmattd@gmail.com>
2020-03-18 22:01:46 -0700

    small changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import React, { Fragment } from 'react';
 import logo from './logo.png';
 import './App.css';
 import * as typeformEmbed from '@typeform/embed'
+import './Fonts.css';
+
 
 
 function App() {
 
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        In this time of physical isolation, everyone deserves to have a pen pal. 
+        <br/>
+        <br/>
 
-        <iframe frameborder="0" height="100%" src="https://koenigmattd.typeform.com/to/IdbKxJ" width="80%" height="350px" data-qa="iframe">
-        </iframe>
-      
-      
-      </header>
+        Scroll down and fill out our form to be matched with a like-minded person to talk politics, the weather, or anything in between.
+
+        </header>
+
+
+
+
+
+
+
+      <iframe frameborder="0" height="100%" src="https://koenigmattd.typeform.com/to/IdbKxJ" width="80%" height="350px" data-qa="iframe">
+      </iframe>
+
+
     </div>
   );
 }
