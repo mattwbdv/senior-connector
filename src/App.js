21406dc0eef4c01b986447e6db5fee8df7ae2b0a
Matt Koenig <koenigmattd@gmail.com>
2020-03-18 21:19:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import logo from './logo.png';
 import './App.css';
+import * as typeformEmbed from '@typeform/embed'
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <iframe frameborder="0" height="100%" src="https://koenigmattd.typeform.com/to/IdbKxJ" width="80%" height="350px" data-qa="iframe">
+        </iframe>
+      
+      
       </header>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/Form.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+ 
+class Series extends React.Component {
+  render() {
+    return <ReactTypeformEmbed url="https://koenigmattd.typeform.com/to/IdbKxJ" />;
+  }
+}
